4427b09aa6efcc2d95b6410872312d0a271365f2
Jim Dowling <jdowling@kth.se>
2014-11-02 07:51:50 +0100

    updated serverspec to 2.0

--- test/integration/default/serverspec/localhost/db_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe command("/var/lib/mysql-cluster/ndb/scripts/mysql-client.sh hop -e \"select count\(*\) from inodes\"") do
-  it { should return_stdout /count/ }
-end
--- test/integration/default/serverspec/localhost/services_spec.rb
+++ test/integration/default/serverspec/localhost/services_spec.rb
@@ -15,4 +15,7 @@ describe service('memcached') do
   it { should be_running   }
 end 
 
+describe command("/var/lib/mysql-cluster/ndb/scripts/mysql-client.sh hop -e \"select count\(*\) from inodes\"") do
+  its (:stdout) { should match /count/ }
+end
 

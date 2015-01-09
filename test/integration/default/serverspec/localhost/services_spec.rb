7ad928497f49660b9ed936535a3dc9891f3b0339
Jim Dowling <jdowling@sics.se>
2015-01-09 11:29:17 +0100

    updated serverspec doesnt use hop db, test db now

--- test/integration/default/serverspec/localhost/services_spec.rb
+++ test/integration/default/serverspec/localhost/services_spec.rb
@@ -15,7 +15,7 @@ describe service('memcached') do
   it { should be_running   }
 end 
 
-describe command("/var/lib/mysql-cluster/ndb/scripts/mysql-client.sh hop -e \"select count\(*\) from inodes\"") do
-  its (:stdout) { should match /count/ }
+describe command("/var/lib/mysql-cluster/ndb/scripts/mysql-client.sh test -e \"show databases\"") do
+  its (:stdout) { should match /test/ }
 end
 

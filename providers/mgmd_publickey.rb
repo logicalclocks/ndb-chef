f69c8ce9ce483f156719b76723fd283b81971be7
Jim Dowling <jdowling@kth.se>
2015-04-20 13:11:56 +0200

    fix recipeName when setting kagent_param

--- providers/mgmd_publickey.rb
+++ providers/mgmd_publickey.rb
@@ -4,10 +4,14 @@ action :set do
  Chef::Log.info "Public key read is: #{contents}"
  node.default[:ndb][:mgmd][:public_key] = "#{contents}"
 
-
+# This works for chef-solo - we are executing this recipe.rb file.
+ recipeName = File.basename("#{__FILE__}")
+ recipeName = File.basename("#{recipeName}", ".rb")
+ 
 kagent_param "/tmp" do
   executing_cookbook "#{cookbook_name}"
-  executing_recipe "#{recipe_name}"
+  #  executing_recipe "#{recipe_name}"
+  executing_recipe  "#{recipeName}"
   cookbook "ndb"
   recipe "mgmd"
   param "public_key"

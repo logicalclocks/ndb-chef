2417e63cd4698457a3130ae65d83e6a15c167493
Jim Dowling <jdowling@sics.se>
2016-01-04 21:48:05 +0100

    Reverting to 7.4.7, due to blocker bug

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,7 +3,7 @@ include_attribute "btsync"
 
 version="7"
 majorVersion="4"
-minorVersion="8"
+minorVersion="7"
 
 versionStr                                          = "#{version}.#{majorVersion}.#{minorVersion}"
 default[:ndb][:enabled]                             = "true"

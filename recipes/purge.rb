7bc4a29482b5f448704466f3f7ba429f1c91c828
Jim Dowling <jdowling@sics.se>
2016-02-09 00:22:57 +0100

    updated purge for systemd scripts

--- recipes/purge.rb
+++ recipes/purge.rb
@@ -1,6 +1,6 @@
 # Stop all the service and remove all services
 # TODO - should rename 'ndbd' as 'ndbmtd'. pkill wont work here fo it
-daemons = %w{ndb_mgmd ndbd mysqld memcached}
+daemons = %w{ndb_mgmd ndbdmtd mysqld memcached}
 daemons.each { |d| 
 
   bash 'kill_running_service_#{d}' do
@@ -8,11 +8,21 @@ daemons.each { |d|
     ignore_failure :true
     code <<-EOF
       service stop #{d}
+      systemctl stop #{d}
       pkill -9 #{d}
     EOF
   end
 
   file "/etc/init.d/#{d}" do
+    action :delete
+    ignore_failure :true
+  end
+  
+  file "/usr/lib/systemd/system/#{d}.service" do
+    action :delete
+    ignore_failure :true
+  end
+  file "/lib/systemd/system/#{d}.service" do
     action :delete
     ignore_failure :true
   end

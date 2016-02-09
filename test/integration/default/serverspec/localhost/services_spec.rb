f59df22eb8a858a55c3a5abdb2436ecbd4d33cf6
%github_username% <jdowling@sics.se>
2016-02-09 07:47:31 +0100

    747 back

--- test/integration/default/serverspec/localhost/services_spec.rb
+++ test/integration/default/serverspec/localhost/services_spec.rb
@@ -10,6 +10,11 @@ describe service('mysqld') do
   it { should be_running   }
 end 
 
+describe service('ndbmtd') do  
+  it { should be_enabled   }
+  it { should be_running   }
+end 
+
 describe service('memcached') do  
   it { should be_enabled   }
   it { should be_running   }

7b05b1d7f2c88b14338f33f9e05b4e25bc608652
Jim Dowling <jdowling@sics.se>
2016-02-29 19:58:18 +0100

    version bump with minor version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ license          "GPL 3.0"
 description      "Installs/Configures NDB (MySQL Cluster)"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-version          "0.1"
+version          "0.2.0"
 
 depends           "kagent"
 

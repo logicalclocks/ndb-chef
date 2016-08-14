9a38565886f8f461c2803568b431b8b564de086b
Jim Dowling <jdowling@sics.se>
2016-08-14 18:25:59 +0200

    fix unit test

--- test/integration/default/serverspec/localhost/services_spec.rb
+++ test/integration/default/serverspec/localhost/services_spec.rb
@@ -10,6 +10,13 @@ describe service('ndbmtd') do
   it { should be_running   }
 end 
 
-describe command("grep -Fxvf /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys") do
+describe service('mysqld') do  
+  it { should be_enabled   }
+  it { should be_running   }
+end 
+
+
+
+describe command("grep -Fxvf /home/mysql/.ssh/id_rsa.pub /home/mysql/.ssh/authorized_keys") do
   its(:exit_status) { should eq 0 }
 end

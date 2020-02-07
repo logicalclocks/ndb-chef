18a2db9f079550ba462136964cb0e650e2e06748
Antonis Kouzoupis <kouzoupis.ant@gmail.com>
2020-02-07 09:46:47 +0100

    [HOPSWORKS-1577] Rescrict allowed connections for admin user (#41)

--- recipes/mysqld_tls.rb
+++ recipes/mysqld_tls.rb
@@ -21,7 +21,6 @@ if node['mysql']['tls'].casecmp?("true")
     found_id=find_service_id("mysqld", node['mysql']['id'])
     my_ip = my_private_ip()
     mysql_ip = node['mysql']['localhost'] == "true" ? "localhost" : my_ip
-
     template "#{node['ndb']['root_dir']}/my.cnf" do
         source "my-ndb.cnf.erb"
         owner node['ndb']['user']
@@ -31,7 +30,7 @@ if node['mysql']['tls'].casecmp?("true")
         variables({
             :mysql_id => found_id,
             :my_ip => mysql_ip,
-            :mysql_tls => true 
+            :mysql_tls => true
     })
     notifies :restart, resources(:service => "mysqld"), :immediately
     end

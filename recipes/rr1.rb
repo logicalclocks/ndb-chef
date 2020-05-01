20dbba2191a53de975a8c237a248b80b39d2645d
Jim Dowling <jdowling@sics.se>
2020-05-01 19:03:11 +0200

    update config.ini in when re-running mgmd.rb recipe

--- recipes/rr1.rb
+++ recipes/rr1.rb
@@ -1,5 +1,9 @@
 service_name = "ndbmtd"
 
+ndb_waiter "rr_wait_mysql_started" do
+  action :wait_until_cluster_ready
+end
+
 service "#{service_name}" do
   provider Chef::Provider::Service::Systemd
   supports :restart => true, :stop => true, :start => true, :status => true
--- recipes/rr14.rb
+++ recipes/rr14.rb
@@ -1,7 +1,12 @@
 service_name = "ndbmtd"
 
+ndb_waiter "rr_wait_mysql_started" do
+  action :wait_until_cluster_ready
+end
+
 service "#{service_name}" do
   provider Chef::Provider::Service::Systemd
   supports :restart => true, :stop => true, :start => true, :status => true
   action :restart
 end
+

146584b4d2834cece961c61c1d7bfd7bfc9189c1
Jim Dowling <jdowling@sics.se>
2020-05-02 07:32:09 +0200

    wait

--- recipes/rr1.rb
+++ recipes/rr1.rb
@@ -1,6 +1,12 @@
+
+# First ndbmtd to start is only waiting on ndb_mgmd, so shorten wait-start timeout
+
+wait_ndbd=node['ndb']['wait_startup']
+
+node.override['ndb']['wait_startup'] = 30
 ndb_waiter "rr_wait_mysql_started" do
   action :wait_until_cluster_ready
 end
-
+node.override['ndb']['wait_startup'] = wait_ndbd
 
 include_recipe "ndb::ndbd"

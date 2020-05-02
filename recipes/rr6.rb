6ccc70898f4a30ea816bb198b8b7c12d1998357c
Jim Dowling <jdowling@sics.se>
2020-05-02 15:44:30 +0200

    need new_node_ids

--- recipes/rr6.rb
+++ recipes/rr6.rb
@@ -1,9 +1,9 @@
 
 # First ndbmtd to start is only waiting on ndb_mgmd, so shorten wait-start timeout
 
-# ndb_waiter "rr_wait_mysql_started" do
-#   nowait_nodes node['ndb']['new_node_ids']
-#   action :wait_until_cluster_ready
-# end
+ndb_waiter "rr_wait_mysql_started" do
+  nowait_nodes node['ndb']['new_node_ids']
+  action :wait_until_cluster_ready
+end
 
 include_recipe "ndb::ndbd"

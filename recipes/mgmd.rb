342d438c8b7a8cc676d68384ba787b1e3395ccc4
Jim Dowling <jdowling@sics.se>
2021-04-12 13:47:23 +0200

    Rondb Fixes (#68)
    * put back kagent/enabled and only register with consul if it is installed
    
    Co-authored-by: Jim Dowling <jim@logicalclocks.com>

--- recipes/mgmd.rb
+++ recipes/mgmd.rb
@@ -148,16 +148,19 @@ kagent_config service_name do
   config_file "#{node['ndb']['root_dir']}/config.ini"
   restart_agent false
   action :add
+  not_if { node['kagent']['enabled'].casecmp("false") == 0 }  
 end
 
 kagent_config "#{service_name}" do
   action :systemd_reload
 end
 
-consul_service "Registering RonDB mgm with Consul" do
-  service_definition "consul/mgm-consul.hcl.erb"
-  reload_consul false
-  action :register
+if exists_local('consul', 'master') or exists_local('consul', 'slave')
+  consul_service "Registering RonDB mgm with Consul" do
+    service_definition "consul/mgm-consul.hcl.erb"
+    reload_consul false
+    action :register
+  end
 end
 
 # Put public key of this mgmd-host in .ssh/authorized_keys of all ndbd and mysqld nodes

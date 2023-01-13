dded661e0ef235946dc8bb1728b0364b234e6de3
Salman Niazi <salman@logicalclocks.com>
2023-01-13 10:04:21 +0100

    HOPSFS-52: Bumped RonDB to 21.04.10

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@ include_attribute "kagent"
 
 default['ndb']['majorVersion']                        = "21"
 default['ndb']['minorVersion']                        = "04"
-default['ndb']['patchVersion']                        = "9"
+default['ndb']['patchVersion']                        = "10"
 
 default['ndb']['version']                             = "#{node['ndb']['majorVersion']}.#{node['ndb']['minorVersion']}.#{node['ndb']['patchVersion']}"
 default['ndb']['enabled']                             = "true"

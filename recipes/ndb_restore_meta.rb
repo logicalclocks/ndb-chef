d2112f3b2c30983a5db724ddda3a16542b8572a2
Antonios Kouzoupis <antonios@logicalclocks.com>
2023-02-07 17:22:51 +0100

    [CLOUD-365] Restore backup
    [CLOUD-365] Fixes

---
+++ recipes/ndb_restore_meta.rb
@@ -0,0 +1,21 @@
+my_node_id = find_service_id("ndbd", 1)
+
+# Don't ask
+ndb_connectstring()
+mgm_connection = node['ndb']['connectstring']
+backup_id = 1
+backup_directory = node['ndb']['local_backup_dir']
+exclude_databases="glassfish_timers.EJB__TIMER__TBL"
+private_ip=my_private_ip()
+should_run = private_ip.eql?(node['ndb']['ndbd']['private_ips'].sort[0])
+
+bash 'ndb_restore metadata' do
+    user 'root'
+    group 'root'
+    timeout 18000
+    code <<-EOH
+        #{node['ndb']['scripts_dir']}/restore_backup.sh ndb-restore -p #{backup_directory}/BACKUP/BACKUP-#{backup_id} -n #{my_node_id} -b #{backup_id} -c #{mgm_connection} -e #{exclude_databases} -m META
+    EOH
+    only_if { should_run }
+    not_if { backup_directory.empty? }
+end

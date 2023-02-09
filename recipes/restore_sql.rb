45279d6d431609173d943be522f4130e2420e89a
Antonios Kouzoupis <antonios@logicalclocks.com>
2023-02-09 15:02:19 +0100

    [CLOUD-365] Drop stored procedures and views in case we retry

--- recipes/restore_sql.rb
+++ recipes/restore_sql.rb
@@ -18,8 +18,18 @@ bash 'Remove host certificates' do
     user 'root'
     group 'root'
     code <<-EOH
+        set -e
+        # Drop the procedures and view in case we retry
+        #{mysql_cli} -e "DROP PROCEDURE IF EXISTS hops.simpleproc"
+        #{mysql_cli} -e "DROP PROCEDURE IF EXISTS hops.flyway"
+        #{mysql_cli} -e "DROP PROCEDURE IF EXISTS airflow.create_idx"
+        #{mysql_cli} -e "DROP VIEW IF EXISTS hopsworks.users_groups"
+
+        #{mysql_cli} -e "DELETE FROM hopsworks.pki_certificate WHERE subject REGEXP '^C=.+ST=Sweden.+CN.+' AND status=1"
         #{mysql_cli} -e "UPDATE hopsworks.pki_certificate SET status=1 WHERE subject REGEXP '^C=.+ST=Sweden.+CN.+'"
-        #{mysql_cli} -e "UPDATE hopsworks.pki_certificate SET status=1 WHERE subject REGEXP '^CN=#{hopsworks_consul}.+';"
+
+        #{mysql_cli} -e "DELETE FROM hopsworks.pki_certificate WHERE subject REGEXP '^CN=#{hopsworks_consul}.+' AND status=1"
+        #{mysql_cli} -e "UPDATE hopsworks.pki_certificate SET status=1 WHERE subject REGEXP '^CN=#{hopsworks_consul}.+'"
     EOH
     only_if { should_run }
     not_if { backup_directory.empty? }

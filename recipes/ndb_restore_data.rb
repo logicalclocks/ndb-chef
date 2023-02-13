4f6628f2e9ac69f60ac695a9c6cc5bd0ad02c483
Antonis Kouzoupis <antonios@logicalclocks.com>
2023-02-13 14:21:10 +0100

    [CLOUD-365] Restore RonDB backup as part of installation (#118)
    * [CLOUD-365] Fixes
    
    * [CLOUD-365] Remove previous host certificates
    
    * [CLOUD-365] Fixes
    
    * [CLOUD-365] Drop stored procedures and views in case we retry
    
    * [CLOUD-365] Choose a shorter NDB backup id
    
    * [CLOUD-365] Add default value for staging backup directory
    
    * [CLOUD-365] Add backup id to tarball
    
    * [CLOUD-365] Update documentation to account for multiple LDM threads

--- recipes/ndb_restore_data.rb
+++ recipes/ndb_restore_data.rb
@@ -3,15 +3,15 @@ my_node_id = find_service_id("ndbd", 1)
 # Don't ask
 ndb_connectstring()
 mgm_connection = node['ndb']['connectstring']
-backup_id = 1
-backup_directory = node['ndb']['local_backup_dir']
+backup_id = node['ndb']['restore']['backup_id']
+backup_directory = node['ndb']['restore']['directory']
 exclude_databases="glassfish_timers.EJB__TIMER__TBL,hopsworks.hosts,hopsworks.host_services"
 
 bash 'ndb_restore data' do
     user 'root'
     group 'root'
     code <<-EOH
-        #{node['ndb']['scripts_dir']}/restore_backup.sh ndb-restore -p #{backup_directory}/BACKUP/BACKUP-#{backup_id} -n #{my_node_id} -b #{backup_id} -c #{mgm_connection} -e #{exclude_databases} -m DATA
+        #{node['ndb']['scripts_dir']}/restore_backup.sh ndb-restore -p #{backup_directory} -n #{my_node_id} -b #{backup_id} -c #{mgm_connection} -e #{exclude_databases} -m DATA
     EOH
     not_if { backup_directory.empty? }
 end

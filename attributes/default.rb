9cf23d078d9ce0fc5bf57f4dcbd759dc78185457
Antonis Kouzoupis <antonios@logicalclocks.com>
2023-07-20 15:12:17 +0200

    [HWORKS-667] Change default location of X plugin socket (#150)
    * [HWORKS-667] Change default location of X plugin socket
    
    * [HWORKS-667] Attribute to override mysqlx port

--- attributes/default.rb
+++ attributes/default.rb
@@ -192,7 +192,9 @@ default['mysql']['version_dir']                       = "#{node['mysql']['base_d
 
 # Location for the MySQL socket - needs to be in a directory which is accessible only to the mysql user
 default['ndb']['mysql_socket']                        = "#{node['ndb']['root_dir']}/mysql.sock"
+default['ndb']['mysqlx_socket']                       = "#{node['ndb']['root_dir']}/mysqlx.sock"
 default['ndb']['mysql_port']                          = "3306"
+default['ndb']['mysqlx_port']                         = "33060"
 
 default['mysql']['localhost']                         = "false"
 

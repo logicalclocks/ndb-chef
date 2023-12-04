d8ae02057b3fa2a1d78e9a5e72cb4c1283afa228
Antonis Kouzoupis <antonios@logicalclocks.com>
2023-12-04 15:45:19 +0100

    [HWORKS-873] Add option to delete existing backup with the same ID (#178)
    [HWORKS-873] Delete backup tarball with the same ID

--- recipes/download_backup.rb
+++ recipes/download_backup.rb
@@ -1,5 +1,6 @@
 primary_mgm = node['ndb']['backup']['private_ips'][0]
 backup_tarball = node['ndb']['restore']['tarball']
+basename = File.basename(node['ndb']['restore']['tarball'], ".tar.gz")
 
 bash 'copy-backup-from-primary' do
     user node['ndb']['user']
@@ -7,6 +8,8 @@ bash 'copy-backup-from-primary' do
     code <<-EOF
         set -e
         pushd #{node['ndb']['local_backup_dir']}
+        rm -f #{backup_tarball}
+        rm -rf #{basename}
         scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null #{node['ndb']['user']}@#{primary_mgm}:#{node['ndb']['local_backup_dir']}/#{backup_tarball} .
         tar xzf #{backup_tarball}
         popd

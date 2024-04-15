58f30191fec0e23d118bd15bf2e1968e9e1615a4
Antonis Kouzoupis <antonios@logicalclocks.com>
2024-04-16 01:38:40 +0200

    [HWORKS-1057] MySQL safe upgrade and recipe to commit upgrade (#194)
    * [HWORKS-1057] Generate my.conf variables in one place
    
    * [HWORKS-1057] Recipe to commit upgrade
    
    * [HWORKS-1057] Fix calculating mysql id

--- recipes/mysqld_tls.rb
+++ recipes/mysqld_tls.rb
@@ -1,5 +1,3 @@
-require 'time'
-
 # This recipe is here to reconfigure the MySQL server to use TLS (if TLS is enabled)
 # It cannot be done in the mysqld.rb recipe as we need MySQL to start Hopsworks to start the CA
 # That's why this recipe is run after kagent::default (responsible for signing the certificates)
@@ -27,28 +25,15 @@ if node['mysql']['tls'].casecmp?("true")
         action :nothing
     end
 
-    server_id = get_mysql_server_id()
+    mycnf_conf = mysqld_configuration(true)
 
-    found_id=find_service_id("mysqld", node['mysql']['id'])
-    certificate = "#{crypto_dir}/#{x509_helper.get_certificate_bundle_name(node['ndb']['user'])}"
-    key = "#{crypto_dir}/#{x509_helper.get_private_key_pkcs1_name(node['ndb']['user'])}"
-    hops_ca = "#{crypto_dir}/#{x509_helper.get_hops_ca_bundle_name()}"
     template "#{node['ndb']['root_dir']}/my.cnf" do
         source "my-ndb.cnf.erb"
         owner node['ndb']['user']
         group node['ndb']['group']
         mode "0640"
         action :create
-        variables({
-            :mysql_id => found_id,
-            :mysql_tls => true,
-            :certificate => certificate,
-            :key => key,
-            :hops_ca => hops_ca,
-            :timezone => Time.now.strftime("%:z"),
-            :server_id => server_id,
-            :am_i_primary => node['ndb']['replication']['role'].casecmp?('primary')
-    })
+        variables(mycnf_conf)
     notifies :restart, resources(:service => "mysqld"), :immediately
     end
 end

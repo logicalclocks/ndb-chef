8b961c7c75ff7d9be53e010345b45d54fc8af337
Antonis Kouzoupis <antonios@logicalclocks.com>
2024-04-26 10:00:40 +0200

    [HWORKS-1179] Avoid deleting Hopsworks certificates when restoring a database backup unless if it's a disaster recovery (#201)
    * [HWORKS-1179] Generalize the deletion of stored procedures and views. Unfortunately you cannot drop with wildcard
    
    * [HWORKS-1179] Parameterize revokation of Host certificates when restoring a backup
    
    * [HWORKS-1179] Temporary solution to RONDB-647 Restart mysqld to synchronize table
    
    * [HWORKS-1179] Stream bash output to Chef logs

--- recipes/restore_sql.rb
+++ recipes/restore_sql.rb
@@ -4,6 +4,9 @@ ndb_connectstring()
 mgm_connection = node['ndb']['connectstring']
 should_run = private_ip.eql?(node['ndb']['restore_sql']['private_ips'].sort[0])
 mysql_cli = "#{node['ndb']['scripts_dir']}/mysql-client.sh"
+mysql_socket = node['ndb']['mysql_socket']
+mysql_client = "#{node['mysql']['bin_dir']}/mysql"
+
 exclude_databases_option = ""
 unless node['ndb']['restore']['exclude_databases_meta'].empty?
     exclude_databases_option = "-e #{node['ndb']['restore']['exclude_databases_meta']}"
@@ -13,6 +16,7 @@ rebuild_indexes_check = "#{Chef::Config['file_cache_path']}/rondb_rebuild_indexe
 bash 'Rebuild indexes' do
     user 'root'
     group 'root'
+    live_stream true
     code <<-EOH
         set -e
         #{node['ndb']['scripts_dir']}/restore_backup.sh ndb-restore -p #{backup_directory} -n 1 -b #{node['ndb']['restore']['backup_id']} -c #{mgm_connection} #{exclude_databases_option} -m REBUILD-INDEXES
@@ -25,16 +29,37 @@ bash 'Rebuild indexes' do
     only_if { rondb_restoring_backup() }
 end
 
+# Temporary fix for the GRANTS propagation issue
+# https://hopsworks.atlassian.net/browse/RONDB-647
+# restarting mysqld will syncronize the tables
+systemd_unit "mysqld.service" do
+    action [:restart]
+end
+
 bash 'Restore SQL' do
     user 'root'
     group 'root'
+    live_stream true
     code <<-EOH
         set -e
-        # Drop the procedures and view in case we retry
-        #{mysql_cli} -e "DROP PROCEDURE IF EXISTS hops.simpleproc"
-        #{mysql_cli} -e "DROP PROCEDURE IF EXISTS hops.flyway"
-        #{mysql_cli} -e "DROP PROCEDURE IF EXISTS airflow.create_idx"
-        #{mysql_cli} -e "DROP VIEW IF EXISTS hopsworks.users_groups"
+        # Drop existing procedures as they will be restored from the backup
+        stored_procs=$(#{mysql_client} -S #{mysql_socket} -Nse "SELECT routine_schema, routine_name FROM information_schema.routines WHERE routine_schema IN ('airflow', 'hopsworks', 'hops')" | awk '{c=$1"."$2; print c}')
+        stored_procs=$(echo $stored_procs | sed 's/[[:space:]]/,/g')
+        for p in ${stored_procs//,/ }
+        do
+            echo "Dropping stored procedure $p"
+            #{mysql_cli} -e "DROP PROCEDURE IF EXISTS $p"
+        done
+
+
+        # Drop existing procedures as they will be restored from the backup
+        views=$(#{mysql_client} -S #{mysql_socket} -Nse "SELECT TABLE_SCHEMA, TABLE_NAME FROM information_schema.tables WHERE TABLE_TYPE = 'VIEW' and TABLE_SCHEMA IN ('airflow', 'hopsworks', 'hops')" | awk '{c=$1"."$2; print c}')
+        views=$(echo $views | sed 's/[[:space:]]/,/g')
+        for p in ${views//,/ }
+        do
+            echo "Dropping view $p"
+            #{mysql_cli} -e "DROP VIEW IF EXISTS $p"
+        done
 
         #{node['ndb']['scripts_dir']}/restore_backup.sh restore-schema -p #{backup_directory}
     EOH
@@ -78,6 +103,7 @@ bash 'Remove host certificates' do
             echo "Hopsworks database DOES NOT exist"
         fi
     EOH
+    only_if { node['ndb']['restore']['revoke_host_certificates'].casecmp?("true") }
     only_if { should_run }
     only_if { rondb_restoring_backup() }
 end
